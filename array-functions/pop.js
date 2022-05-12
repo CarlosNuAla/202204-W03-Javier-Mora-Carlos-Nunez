e922a916a87994f512eb90df6fad9089557923d1
CarlosNuAla <carlosnunezalayon@gmail.com>
2022-05-12 12:59:01 +0100

    Fix pop-function and add test

--- array-functions/pop.js
+++ array-functions/pop.js
@@ -1,15 +1,9 @@
 export const pop = (array = []) => {
-    if (!Array.isArray(array)) {
-        if (array === null || array === undefined) {
-            throw new TypeError('array parameter must be an array');
-        }
-        throw new TypeError('array parameter must be an array');
-    } else if (array.length === 0) {
+    if (array.length === 0) {
         return undefined;
     } else {
         let dropOut = array[array.length - 1];
         array.length = array.length - 1;
-        console.log(array);
         return dropOut;
     }
 }
